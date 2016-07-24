13cb24d25659a5cb6ec67e42ab5b0f943e8a7697
Friedemann Sommer <friedemannsommer@yahoo.de>
2016-07-24 15:42:12 +0200

    changed default registration

--- src/template.ts
+++ src/template.ts
@@ -13,24 +13,24 @@ import filterLowercase from './filter/lowercase'
 import filterUppercase from './filter/uppercase'
 import filterCurrency from './filter/currency'
 
+const defaultHelper: Object = {
+    '??': helperCondition,
+    '-': helperJavascript,
+    '!': helperHTMLEscape,
+    '?': helperCondition,
+    '~': helperIterate,
+    '*': helperComment,
+    '=': helperPrint
+}
+
+const defaultFilter: Object = {
+    'lowercase': filterLowercase,
+    'uppercase': filterUppercase,
+    'currency': filterCurrency
+}
+
 function template(template: string, imports: Object = {}): Templata.Interface.CompileFunction {
-    let Compiler = new TemplateCompiler(imports)
-
-    // register helper
-    Compiler.registerHelper('??', helperCondition)
-    Compiler.registerHelper('-', helperJavascript)
-    Compiler.registerHelper('!', helperHTMLEscape)
-    Compiler.registerHelper('?', helperCondition)
-    Compiler.registerHelper('~', helperIterate)
-    Compiler.registerHelper('*', helperComment)
-    Compiler.registerHelper('=', helperPrint)
-
-    // register filter
-    Compiler.registerFilter('lowercase', filterLowercase)
-    Compiler.registerFilter('uppercase', filterUppercase)
-    Compiler.registerFilter('currency', filterCurrency)
-
-    return Compiler.compile(template)
+    return new TemplateCompiler(imports, defaultHelper, defaultFilter).compile(template)
 }
 
 export default template
