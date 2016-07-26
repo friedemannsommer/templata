71a6f3e9fe5483a2f0e826822ecc8cd5c7569228
Friedemann Sommer <friedemannsommer@yahoo.de>
2016-07-26 11:44:03 +0200

    minor changes for playground

--- web-src/app/index.js
+++ web-src/app/index.js
@@ -1,70 +1,80 @@
-(function(templateCompiler) {
-    // values
-    var template = '<div>Hi {{= local.name =}}!</div>\n<span>\n{{? local.usingTemplata ?}}\nYou`re amazing!\n{{? ?}}\nGet on it!\n{{/?}}\n</span>';
-    var data = { name: "John White", usingTemplata: false };
-    var compiled = '';
-    var result = '';
-    // compiler instance
-    var compiler = templateCompiler(template);
+(function (templateCompiler) {
     // elements
     var templateArea = document.getElementById('template');
     var templateData = document.getElementById('data');
     var templateCompiled = document.getElementById('compiled');
     var templateResult = document.getElementById('result');
     // additional variables
-    var timer;
+    var compiler;
+    var userData;
     // set inital values
-    templateArea.value = template;
-    templateData.value = JSON.stringify(data);
-    // setup
-    renderTemplate();
+    try {
+        userData = JSON.parse(templateData.value);
+    } catch (e) {
+        alert('[Templata] ' + e.message);
+    }
+
+    if (userData) {
+        createTemplate(templateArea.value, userData);
+    }
     // add listener
-    templateArea.addEventListener('keyup', function(e) {
-        if (!!timer) {
-            clearTimeout(timer);
-        }
-
-        timer = setTimeout(function() {
-            onUpdate('template', e);
-        }, 500);
-    }, false);
-    templateData.addEventListener('keyup', function(e) {
-        if (!!timer) {
-            clearTimeout(timer);
-        }
-
-        timer = setTimeout(function() {
-            onUpdate('data', e);
-        }, 500);
-    }, false);
+    templateArea.addEventListener('keyup', function (e) {
+        onUpdate('template', e);
+    }, { capture: false, passive: true });
+    templateData.addEventListener('keyup', function (e) {
+        onUpdate('data', e);
+    }, { capture: false, passive: true });
 
     function onUpdate(type, e) {
-        var errorOccurred = false;
-
         switch (type) {
             case 'template':
-                template = e.target.value;
-                compiler = templateCompiler(template);
-                renderTemplate();
+                createTemplate(e.target.value);
                 break;
             case 'data':
-                try {
-                    data = JSON.parse(e.target.value);
-                } catch (e) {
-                    errorOccurred = true;
-                }
-
-                if (!errorOccurred) {
-                    renderTemplate();
-                }
+                parseData(e.target.value);
                 break;
             default:
                 break;
         }
     }
 
-    function renderTemplate() {
+    function parseData(inputData) {
+        var errorOccurred = false;
+        var data;
+
+        try {
+            data = JSON.parse(inputData);
+        } catch (e) {
+            errorOccurred = true;
+
+            if (console && console.log) {
+                console.log('[Templata] ' + e.message);
+            }
+        }
+
+        if (!errorOccurred) {
+            renderTemplate(data);
+        }
+    }
+
+    function createTemplate(template, data) {
+        compiler = templateCompiler(template);
         templateCompiled.value = compiler.toString();
+
+        if (!!data) {
+            renderTemplate(data);
+        } else {
+            renderTemplate(data);
+        }
+    }
+
+    function renderTemplate(data) {
+        if (!data) {
+            data = userData;
+        } else {
+            userData = data;
+        }
+
         templateResult.value = compiler(data);
     }
 })(window.Templata.default);
